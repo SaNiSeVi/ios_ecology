26aafb7423245440c5fd1b9843ae706bc029099e
Vitya <arteezycat@gmail.com>
2020-11-09 20:13:37 +0300

    Added visualization of initial screens

--- EcoNews/EcoNews/SignUpControllerView.swift
+++ EcoNews/EcoNews/SignUpControllerView.swift
@@ -17,10 +17,25 @@ class SignUpControllerView: UIViewController {
     @IBOutlet weak var passwordTextField:UITextField!
     @IBOutlet weak var errorLabel: UILabel!
     
+    @IBOutlet weak var buttonForSign: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        assignbackground()
         // Do any additional setup after loading the view.
+    }
+    
+    func assignbackground(){
+        let background = UIImage(named: "signUp.png")
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+        self.buttonForSign.layer.cornerRadius = 3
     }
     
     func checkValidData()->String?{
